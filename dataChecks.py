c9bb1956cc0a6590980d9f9a883e1d7bfea6bad1
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-07-25 13:58:09 +0100

    add data and the checks.py file

---
+++ dataChecks.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_csv('MOCK_DATA.csv')
+
+print(df.info())
+print(df.describe())
+print(df.describe(include=object))
+print('\n\n\nNumber of nulls:\n')
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df.drop_duplicates())
